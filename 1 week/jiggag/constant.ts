0a49128cf8ec7636030f8760510378d58107efaa
jiggag <jiggag90@gmail.com>
2019-09-25 16:59:19 +0900

    [1week] constant

---
+++ 1 week/jiggag/constant.ts
@@ -0,0 +1,13 @@
+
+  const constant = (a: string | number) => {
+    return (_?: typeof globalThis) => a;
+  };
+  
+  const constantA = constant('A');
+  const constantNum = constant(1);
+  const constantStr = constant('상수');
+  
+  console.log(constantA()); // A
+  console.log(constantNum()); // 1
+  console.log(constantStr()); // 상수
+  
