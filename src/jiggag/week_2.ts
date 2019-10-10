cb23648bd04a145a059a48cca2b45134549344d5
jiggag <jiggag90@gmail.com>
2019-10-10 16:32:12 +0900

    [2week] curryN

--- src/jiggag/week_2.ts
+++ src/jiggag/week_2.ts
@@ -1,3 +1,11 @@
 import delay from './delay';
+import curryN from './curryN';
 
+// delay
 delay(1000,  console.log("1초 후 로그"));
+
+// curryN
+const func = curryN(3, (...args) => args.join('-'));
+console.log(func('함수')('첫번째')('두번째')('세번째'));
+const func2 = curryN(3, (...args) => args.reduce((a, b) => a + b));
+console.log(func2(2)(4)(1)(1));
